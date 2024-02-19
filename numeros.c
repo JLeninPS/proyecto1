7757d6ad70b929529ecfd50fdc492eea777ddd2b
JLeninPS <pocoacalenin@gmail.com>
2024-02-18 20:03:07 -0400

    PrimerCommit

---
+++ numeros.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+
+int main(){
+    int x = 15;
+    float y = 15.456;
+    double z = 171.2163478129;
+
+    printf("Entero: %d\n", x);
+    printf("Flotante: %f\n", y);
+    printf("Double: %lf\n", z);
+    
+}
