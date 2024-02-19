6c80b2ff646a302ccd926f215de9a321edf650d3
JLeninPS <pocoacalenin@gmail.com>
2024-02-19 10:01:19 -0400

    Se agrego myChar3 como ascii a caracteres.c

--- caracteres.c
+++ caracteres.c
@@ -3,5 +3,7 @@
 int main(){
     char myChar = 'A';
     char myChar2 = 'B';
+    char myChar3 = 68;
     printf("Caracter: %c\nCaracter: %c\n", myChar, myChar2);
+    printf("Caracter(ASCII): %c\n", myChar3);
 }
