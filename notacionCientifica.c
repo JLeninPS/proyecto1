67a664acf9fe62cac4df2db193a80770f53344bb
JLeninPS <pocoacalenin@gmail.com>
2024-02-21 11:30:23 -0400

    Se agrego archivo notacionCientifica.c

---
+++ notacionCientifica.c
@@ -0,0 +1,11 @@
+#include"stdio.h"
+
+void main(){
+    int x = 4e3;
+    float y = 3.4e4;
+
+    printf("\tNOTACION CINETIFICA\n");
+    printf("Entero: %d\n", x); 
+    printf("Flotante: %f\n", y);
+
+}
